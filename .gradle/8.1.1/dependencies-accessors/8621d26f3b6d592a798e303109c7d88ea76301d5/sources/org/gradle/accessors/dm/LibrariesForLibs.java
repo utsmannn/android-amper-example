871987171854f5e64fdaa789563f6c5b45c9d7e4
utsmannn <utsmannn@gmail.com>
2023-11-24 22:00:17 +0700

    initial commit

---
+++ .gradle/8.1.1/dependencies-accessors/8621d26f3b6d592a798e303109c7d88ea76301d5/sources/org/gradle/accessors/dm/LibrariesForLibs.java
@@ -0,0 +1,217 @@
+package org.gradle.accessors.dm;
+
+import org.gradle.api.NonNullApi;
+import org.gradle.api.artifacts.MinimalExternalModuleDependency;
+import org.gradle.plugin.use.PluginDependency;
+import org.gradle.api.artifacts.ExternalModuleDependencyBundle;
+import org.gradle.api.artifacts.MutableVersionConstraint;
+import org.gradle.api.provider.Provider;
+import org.gradle.api.model.ObjectFactory;
+import org.gradle.api.provider.ProviderFactory;
+import org.gradle.api.internal.catalog.AbstractExternalDependencyFactory;
+import org.gradle.api.internal.catalog.DefaultVersionCatalog;
+import java.util.Map;
+import org.gradle.api.internal.attributes.ImmutableAttributesFactory;
+import org.gradle.api.internal.artifacts.dsl.CapabilityNotationParser;
+import javax.inject.Inject;
+
+/**
+ * A catalog of dependencies accessible via the `libs` extension.
+ */
+@NonNullApi
+public class LibrariesForLibs extends AbstractExternalDependencyFactory {
+
+    private final AbstractExternalDependencyFactory owner = this;
+    private final AndroidLibraryAccessors laccForAndroidLibraryAccessors = new AndroidLibraryAccessors(owner);
+    private final KotlinLibraryAccessors laccForKotlinLibraryAccessors = new KotlinLibraryAccessors(owner);
+    private final KtorClientLibraryAccessors laccForKtorClientLibraryAccessors = new KtorClientLibraryAccessors(owner);
+    private final VersionAccessors vaccForVersionAccessors = new VersionAccessors(providers, config);
+    private final BundleAccessors baccForBundleAccessors = new BundleAccessors(objects, providers, config, attributesFactory, capabilityNotationParser);
+    private final PluginAccessors paccForPluginAccessors = new PluginAccessors(providers, config);
+
+    @Inject
+    public LibrariesForLibs(DefaultVersionCatalog config, ProviderFactory providers, ObjectFactory objects, ImmutableAttributesFactory attributesFactory, CapabilityNotationParser capabilityNotationParser) {
+        super(config, providers, objects, attributesFactory, capabilityNotationParser);
+    }
+
+    /**
+     * Returns the group of libraries at android
+     */
+    public AndroidLibraryAccessors getAndroid() {
+        return laccForAndroidLibraryAccessors;
+    }
+
+    /**
+     * Returns the group of libraries at kotlin
+     */
+    public KotlinLibraryAccessors getKotlin() {
+        return laccForKotlinLibraryAccessors;
+    }
+
+    /**
+     * Returns the group of libraries at ktorClient
+     */
+    public KtorClientLibraryAccessors getKtorClient() {
+        return laccForKtorClientLibraryAccessors;
+    }
+
+    /**
+     * Returns the group of versions at versions
+     */
+    public VersionAccessors getVersions() {
+        return vaccForVersionAccessors;
+    }
+
+    /**
+     * Returns the group of bundles at bundles
+     */
+    public BundleAccessors getBundles() {
+        return baccForBundleAccessors;
+    }
+
+    /**
+     * Returns the group of plugins at plugins
+     */
+    public PluginAccessors getPlugins() {
+        return paccForPluginAccessors;
+    }
+
+    public static class AndroidLibraryAccessors extends SubDependencyFactory {
+
+        public AndroidLibraryAccessors(AbstractExternalDependencyFactory owner) { super(owner); }
+
+            /**
+             * Creates a dependency provider for viewmodelCompose (androidx.lifecycle:lifecycle-viewmodel-compose)
+             * This dependency was declared in catalog libs.versions.toml
+             */
+            public Provider<MinimalExternalModuleDependency> getViewmodelCompose() {
+                return create("android.viewmodelCompose");
+        }
+
+            /**
+             * Creates a dependency provider for viewmodelKtx (androidx.lifecycle:lifecycle-viewmodel-ktx)
+             * This dependency was declared in catalog libs.versions.toml
+             */
+            public Provider<MinimalExternalModuleDependency> getViewmodelKtx() {
+                return create("android.viewmodelKtx");
+        }
+
+    }
+
+    public static class KotlinLibraryAccessors extends SubDependencyFactory {
+
+        public KotlinLibraryAccessors(AbstractExternalDependencyFactory owner) { super(owner); }
+
+            /**
+             * Creates a dependency provider for coroutine (org.jetbrains.kotlinx:kotlinx-coroutines-core)
+             * This dependency was declared in catalog libs.versions.toml
+             */
+            public Provider<MinimalExternalModuleDependency> getCoroutine() {
+                return create("kotlin.coroutine");
+        }
+
+    }
+
+    public static class KtorClientLibraryAccessors extends SubDependencyFactory {
+
+        public KtorClientLibraryAccessors(AbstractExternalDependencyFactory owner) { super(owner); }
+
+            /**
+             * Creates a dependency provider for contentNegotiation (io.ktor:ktor-client-content-negotiation)
+             * This dependency was declared in catalog libs.versions.toml
+             */
+            public Provider<MinimalExternalModuleDependency> getContentNegotiation() {
+                return create("ktorClient.contentNegotiation");
+        }
+
+            /**
+             * Creates a dependency provider for core (io.ktor:ktor-client-core)
+             * This dependency was declared in catalog libs.versions.toml
+             */
+            public Provider<MinimalExternalModuleDependency> getCore() {
+                return create("ktorClient.core");
+        }
+
+            /**
+             * Creates a dependency provider for logging (io.ktor:ktor-client-logging)
+             * This dependency was declared in catalog libs.versions.toml
+             */
+            public Provider<MinimalExternalModuleDependency> getLogging() {
+                return create("ktorClient.logging");
+        }
+
+            /**
+             * Creates a dependency provider for okhttp (io.ktor:ktor-client-okhttp)
+             * This dependency was declared in catalog libs.versions.toml
+             */
+            public Provider<MinimalExternalModuleDependency> getOkhttp() {
+                return create("ktorClient.okhttp");
+        }
+
+            /**
+             * Creates a dependency provider for serialization (io.ktor:ktor-serialization-kotlinx-json)
+             * This dependency was declared in catalog libs.versions.toml
+             */
+            public Provider<MinimalExternalModuleDependency> getSerialization() {
+                return create("ktorClient.serialization");
+        }
+
+    }
+
+    public static class VersionAccessors extends VersionFactory  {
+
+        private final AndroidxVersionAccessors vaccForAndroidxVersionAccessors = new AndroidxVersionAccessors(providers, config);
+        public VersionAccessors(ProviderFactory providers, DefaultVersionCatalog config) { super(providers, config); }
+
+            /**
+             * Returns the version associated to this alias: coroutine (1.7.3)
+             * If the version is a rich version and that its not expressible as a
+             * single version string, then an empty string is returned.
+             * This version was declared in catalog libs.versions.toml
+             */
+            public Provider<String> getCoroutine() { return getVersion("coroutine"); }
+
+            /**
+             * Returns the version associated to this alias: ktor (2.3.5)
+             * If the version is a rich version and that its not expressible as a
+             * single version string, then an empty string is returned.
+             * This version was declared in catalog libs.versions.toml
+             */
+            public Provider<String> getKtor() { return getVersion("ktor"); }
+
+        /**
+         * Returns the group of versions at versions.androidx
+         */
+        public AndroidxVersionAccessors getAndroidx() {
+            return vaccForAndroidxVersionAccessors;
+        }
+
+    }
+
+    public static class AndroidxVersionAccessors extends VersionFactory  {
+
+        public AndroidxVersionAccessors(ProviderFactory providers, DefaultVersionCatalog config) { super(providers, config); }
+
+            /**
+             * Returns the version associated to this alias: androidx.lifecycle (2.6.2)
+             * If the version is a rich version and that its not expressible as a
+             * single version string, then an empty string is returned.
+             * This version was declared in catalog libs.versions.toml
+             */
+            public Provider<String> getLifecycle() { return getVersion("androidx.lifecycle"); }
+
+    }
+
+    public static class BundleAccessors extends BundleFactory {
+
+        public BundleAccessors(ObjectFactory objects, ProviderFactory providers, DefaultVersionCatalog config, ImmutableAttributesFactory attributesFactory, CapabilityNotationParser capabilityNotationParser) { super(objects, providers, config, attributesFactory, capabilityNotationParser); }
+
+    }
+
+    public static class PluginAccessors extends PluginFactory {
+
+        public PluginAccessors(ProviderFactory providers, DefaultVersionCatalog config) { super(providers, config); }
+
+    }
+
+}
---
+++ src/MainActivity.kt
@@ -0,0 +1,115 @@
+package hello.world
+
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.text.BasicText
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorProducer
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.compose.viewModel
+import io.ktor.client.*
+import io.ktor.client.engine.okhttp.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+
+sealed interface RenderState {
+    object Idle : RenderState
+    object RenderLoading : RenderState
+    data class RenderProduct(val product: String) : RenderState
+    data class RenderFailure(val throwable: Throwable) : RenderState
+}
+
+object WebService {
+    private val client = HttpClient(OkHttp)
+    
+    suspend fun getProductFlow(): Flow<RenderState> {
+
+        return flow {
+            val httpResponse = client.get("https://marketfake.fly.dev/product")
+            if (httpResponse.status.isSuccess()) {
+                emit(RenderState.RenderProduct(httpResponse.bodyAsText()))
+            } else {
+                emit(RenderState.RenderFailure(Throwable(httpResponse.bodyAsText())))
+            }
+        }.catch {
+            emit(RenderState.RenderFailure(it))
+        }.onStart {
+            emit(RenderState.RenderLoading)
+        }
+    }
+}
+
+class MainViewModel : ViewModel() {
+    private val _renderState: MutableStateFlow<RenderState> = MutableStateFlow(RenderState.Idle)
+    val renderState: StateFlow<RenderState> = _renderState.asStateFlow()
+    
+    fun getProduct() = viewModelScope.launch {
+        WebService
+            .getProductFlow()
+            .stateIn(this)
+            .collect(_renderState)
+    }
+}
+
+@Composable
+fun Screen() {
+    val viewModel: MainViewModel = viewModel()
+    val state by viewModel.renderState.collectAsState()
+    
+    LaunchedEffect(Unit) {
+        viewModel.getProduct()
+    }
+
+    MaterialTheme {
+        Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            when (val value = state) {
+                is RenderState.RenderLoading -> {
+                    CircularProgressIndicator()
+                }
+                is RenderState.RenderProduct -> {
+                    BasicText(
+                        text = value.product
+                    )
+                }
+                is RenderState.RenderFailure -> {
+                    BasicText(
+                        text = value.throwable.localizedMessage.orEmpty(),
+                        color = ColorProducer {
+                            Color.Red
+                        }
+                    )
+                }
+                else -> {}
+            }
+        }
+    }
+}
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Screen()
+        }
+    }
+}
